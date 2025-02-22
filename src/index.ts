7e150cc515698f727d505b96411fea637ebeda90
deethesaint <vtduong0912@gmail.com>
2025-02-22 20:27:10 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
+// Add Express
 const express = require("express");
+
+// Initialize Express
 const app = express();
 
-app.get('/', (req: any, res: any) => res.send("Momento On Vercel"));
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
 
-app.listen(3000, () => console.log('Server ready on port 3000.'));
+// Initialize server
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
 
 module.exports = app;
