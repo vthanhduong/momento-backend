02cebf49b93326cb5c1eed82f9cb48d4217fc66c
deethesaint <vtduong0912@gmail.com>
2025-02-23 09:31:04 +0700

    update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ const app = express();
 
 // Create GET request
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.status(400).json({
+    status: "success",
+    message: "Momento",
+  })
 });
 
 // Initialize server
