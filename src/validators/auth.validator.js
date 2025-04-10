dfbb1448fccbe0fdf57cf6ca5f28218360f75c92
deethesaint <vtduong0912@gmail.com>
2025-04-10 10:43:24 +0700

    update: auth validator

---
+++ src/validators/auth.validator.js
@@ -0,0 +1,33 @@
+const {check} = require("express-validator");
+
+let validateRegisteredUser = () => {
+    return [
+        check("username", "Username must not be empty").not().isEmpty(),
+        check("username", "Username must be an email").isEmail(),
+        check("username", "Username must be alphanumeric").isAlphanumeric(),
+        check("username", "Username must be 6 characters minimum length at least").isLength({min: 6}),
+        check("password", "Password must not be empty").not().isEmpty(),
+        check("password", "Password must be 6 characters minimum length at least").isLength({min: 6}),
+    ];
+}
+
+let validateLogin = () => {
+    return [
+        check("username", "Username must not be empty").not().isEmpty(),
+        check("username", "Username must be an email").isEmail(),
+        check("password", "Password must not be empty").not().isEmpty(),
+    ];
+}
+
+let validateChangedPassword = () => {
+    return [
+
+    ]
+}
+
+let validate = {
+    validateRegisteredUser: validateRegisteredUser,
+    validateLogin: validateLogin,
+}
+
+module.exports = {validate}
