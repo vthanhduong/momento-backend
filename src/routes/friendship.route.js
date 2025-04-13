acf2024d792f38ac47aa6bd8a880b4ed29e73a46
deethesaint <vtduong0912@gmail.com>
2025-04-14 00:00:34 +0700

    update momemnts

--- src/routes/friendship.route.js
+++ src/routes/friendship.route.js
@@ -6,6 +6,8 @@ const router = express();
 router.get("/get-friends", isAuthorized, controller.getFriends);
 router.post("/add-friend/:id", isAuthorized, controller.addFriendWith);
 router.post("/accept-friend/:id", isAuthorized, controller.acceptFriend);
-router.post("/unfriend/:id", isAuthorized, controller.unfriend);
+router.delete("/unfriend/:id", isAuthorized, controller.unfriend);
 router.get("/get-friend-invitations", isAuthorized, controller.getFriendInvitations);
+router.delete("/reject-invitation/:id", isAuthorized, controller.rejectInvitations);
+
 module.exports = router;
